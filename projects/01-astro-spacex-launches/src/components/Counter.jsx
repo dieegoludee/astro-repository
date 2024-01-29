f5bc2c1bf1ca6f59259d09796aacc57b27366217
Diego Ludeña <diegolude.martin@gmail.com>
2024-01-29 17:41:34 +0100

    SpaceX Luanches App con Astro

---
+++ projects/01-astro-spacex-launches/src/components/Counter.jsx
@@ -0,0 +1,23 @@
+import { useState } from "preact/hooks";
+
+export function Counter() {
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <div class="text-center mb-4">
+      <span class="text-yellow-400 text-xl mr-4">{counter}</span>
+      <button 
+        class="border px-4 py-2 text-xl"
+        onClick={() => setCounter(counter => counter + 1)}
+      >
+        +
+      </button>
+      <button 
+        class="border px-4 py-2 text-xl"
+        onClick={() => setCounter(counter => counter - 1)}
+      >
+        -
+      </button>
+    </div>
+  )
+}
